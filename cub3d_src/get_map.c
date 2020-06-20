8090baf799885ca062a0382ebf34ae9bf3f9f98d
Nikita <nikitos_athletos@mail.ru>
2020-06-20 21:40:44 +0500

    added get_map and get_map_data file

---
+++ cub3d_src/get_map.c
@@ -0,0 +1,112 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_map.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: imicah <imicah@student.21-school.ru>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/20 21:36:56 by imicah            #+#    #+#             */
+/*   Updated: 2020/06/20 21:36:57 by imicah           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub3d.h"
+
+void			*copy_read_map(char **map, char *line, unsigned short *i)
+{
+	while (*line)
+	{
+		if (ft_strchr(MAP_CHAR, *line))
+			(*map)[(*i)++] = *line++;
+		else
+		{
+			free(map);
+			return (NULL);
+		}
+	}
+	(*map)[(*i)++] = '\n';
+	(*map)[*i] = 0;
+	return ((void*)1);
+}
+
+char			*read_map(int fd, char **line)
+{
+	char			*map;
+	unsigned short	i;
+
+	i = 0;
+	map = NULL;
+	free(*line);
+	while (get_next_line(fd, line) > 0)
+	{
+		if (**line)
+		{
+			map = ft_realloc(map, (int)ft_strlen(*line) + 2);
+			if (copy_read_map(&map, *line, &i) == NULL)
+				return (NULL);
+		}
+		free(*line);
+	}
+	free(*line);
+	return (map);
+}
+
+unsigned short	count_data(char *map_temp, unsigned short *lengh_line)
+{
+	unsigned short 	i;
+	unsigned short	q;
+	unsigned short	count_line;
+
+	i = 0;
+	*lengh_line = 0;
+	count_line = 0;
+	while (map_temp[i])
+	{
+		if ((q = (short)ft_strchr(map_temp + i, '\n')) > *lengh_line)
+			*lengh_line = q - 1;
+		count_line++;
+		if (q == 0)
+			return (--count_line);
+		i += q;
+	}
+	return (count_line);
+}
+
+char			*copy_write_map(char *map, char *map_temp, unsigned short length_line)
+{
+	unsigned short i;
+	unsigned short q;
+	unsigned short k;
+
+	i = 0;
+	q = 0;
+	k = 0;
+	while (map_temp[i])
+	{
+		if (map_temp[i] == '\n')
+		{
+			while (k++ < length_line)
+				map[q++] = ' ';
+			i++;
+			k = 0;
+		}
+		map[q++] = map_temp[i++];
+		k++;
+	}
+	map[--q] = 0;
+	return (map);
+}
+
+char		write_map(int fd, char **line, char **map, unsigned short *length_line)
+{
+	char			*map_temp;
+	unsigned short	count_line;
+
+	if ((map_temp = read_map(fd, line)) == NULL)
+		return (0);
+	count_line = count_data(map_temp, length_line);
+	*map = ft_calloc(sizeof(char), (*length_line * count_line) + 1);
+	*map = copy_write_map(*map, map_temp, *length_line);
+	free(map_temp);
+	return (1);
+}
---
+++ cub3d_src/get_map_data.c
@@ -0,0 +1,85 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_map_data.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: imicah <imicah@student.21-school.ru>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/20 21:36:50 by imicah            #+#    #+#             */
+/*   Updated: 2020/06/20 21:36:51 by imicah           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub3d.h"
+
+char 	get_texture(char *line, char **texture)
+{
+	while (*line == ' ')
+		line++;
+	*texture = ft_strdup(line);
+//	if (open(texture, O_RDONLY) == -1)
+//		return (-3);
+	return (1);
+}
+
+char	get_resolution(char *line, short *resolution)
+{
+	line += 2;
+	if ((resolution[0] = (short)ft_atoi(line)) <= 0)
+		return (-2);
+	while ('0' <= *line && *line <= '9')
+		line++;
+	if ((resolution[1] = (short)ft_atoi(line)) <= 0)
+		return (-2);
+	return (1);
+}
+
+char	get_color(char *line, unsigned char *color)
+{
+	int 	i;
+	int 	color_value;
+	int 	flag;
+
+	i = 0;
+	while (*line)
+	{
+		if ((('0' <= *line && *line <= '9') || *line == '-') && flag)
+		{
+			color_value = ft_atoi(line);
+			flag = 0;
+			if (0 <= color_value && color_value <= 255)
+				color[i++] = color_value;
+			else
+				return (-1);
+		}
+		else if (!(('0' <= *line && *line <= '9') || *line == '-'))
+			flag = 1;
+		line++;
+	}
+	return (1);
+}
+
+char 	get_map_data(char *line, t_map_data **map_data)
+{
+	char	result;
+
+	result = 0;
+	if (line[0] == 'N' && line[1] == 'O')
+		result = get_texture(line + 2, &(*map_data)->textures[0]);
+	else if (line[0] == 'S' && line[1] == 'O')
+		result = get_texture(line + 2, &(*map_data)->textures[1]);
+	else if (line[0] == 'W' && line[1] == 'E')
+		result = get_texture(line + 2, &(*map_data)->textures[2]);
+	else if (line[0] == 'E' && line[1] == 'A')
+		result = get_texture(line + 2, &(*map_data)->textures[3]);
+	else if (*line == 'S')
+		result = get_texture(line + 2, &(*map_data)->sprite_texture);
+	else if (*line == 'R')
+		result = get_resolution(line, (*map_data)->resolution);
+	else if (*line == 'F')
+		result = get_color(line, (*map_data)->floor_color);
+	else if (*line == 'C')
+		result = get_color(line, (*map_data)->celling_color);
+	free(line);
+	return (result);
+}
