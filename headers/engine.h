af1819ebc8311deab6ba7cbeb32c4fad1b557447
Nikita <nikitos_athletos@mail.ru>
2020-06-25 16:45:56 +0500

    added engine files

---
+++ headers/engine.h
@@ -0,0 +1,17 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   engine.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: imicah <imicah@student.21-school.ru>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/25 16:43:07 by imicah            #+#    #+#             */
+/*   Updated: 2020/06/25 16:43:07 by imicah           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub3d.h"
+
+void 	counting_player_coordinate(char *map, t_player *player);
+void	change_position(int key, t_player *player);
+void	change_pov(int key, t_player *player);
